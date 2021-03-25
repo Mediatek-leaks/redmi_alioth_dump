a4842cb7285e80b388be120fd6fc73de64123680
root <root@fv-az118-341.zpfx1xf0t5ievkbezsmlt2de5b.xx.internal.cloudapp.net>
2021-03-25 15:15:19 +0000

    Add extras for qssi-user 11 RKQ1.200826.002 V12.0.7.0.RKHCNXM release-keys
    Signed-off-by: root <root@fv-az118-341.zpfx1xf0t5ievkbezsmlt2de5b.xx.internal.cloudapp.net>

---
+++ README.md
@@ -0,0 +1,21 @@
+## qssi-user 11 RKQ1.200826.002 V12.0.7.0.RKHCNXM release-keys
+- Manufacturer: xiaomi
+- Platform: kona
+- Codename: alioth
+- Brand: Redmi
+- Flavor: qssi-user
+- Release Version: 11
+- Id: RKQ1.200826.002
+- Incremental: V12.0.7.0.RKHCNXM
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: true
+- Locale: zh-CN
+- Screen Density: 440
+- Fingerprint: Redmi/alioth/alioth:11/RKQ1.200826.002/V12.0.7.0.RKHCNXM:user/release-keys
+- OTA version: 
+- Branch: qssi-user-11-RKQ1.200826.002-V12.0.7.0.RKHCNXM-release-keys
+- Repo: redmi_alioth_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+cat modem/image/sdx55m/qdsp6sw.mbn.* 2>/dev/null >> modem/image/sdx55m/qdsp6sw.mbn
+rm -f modem/image/sdx55m/qdsp6sw.mbn.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat vendor_bootimg/04_dtbdump_c44Q.dtb.* 2>/dev/null >> vendor_bootimg/04_dtbdump_c44Q.dtb
+rm -f vendor_bootimg/04_dtbdump_c44Q.dtb.* 2>/dev/null
+cat bootRE/boot.elf.* 2>/dev/null >> bootRE/boot.elf
+rm -f bootRE/boot.elf.* 2>/dev/null
+cat vendor/camera/mimoji/model2.zip.* 2>/dev/null >> vendor/camera/mimoji/model2.zip
+rm -f vendor/camera/mimoji/model2.zip.* 2>/dev/null
+cat vendor/app/MiGameService/MiGameService.apk.* 2>/dev/null >> vendor/app/MiGameService/MiGameService.apk
+rm -f vendor/app/MiGameService/MiGameService.apk.* 2>/dev/null
+cat vendor/lib64/librelight_only.so.* 2>/dev/null >> vendor/lib64/librelight_only.so
+rm -f vendor/lib64/librelight_only.so.* 2>/dev/null
+cat vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null >> vendor/data-app/SmartHome/SmartHome.apk
+rm -f vendor/data-app/SmartHome/SmartHome.apk.* 2>/dev/null
+cat vendor_boot.img.* 2>/dev/null >> vendor_boot.img
+rm -f vendor_boot.img.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
+cat system/system/apex/com.android.art.release.apex.* 2>/dev/null >> system/system/apex/com.android.art.release.apex
+rm -f system/system/apex/com.android.art.release.apex.* 2>/dev/null
+cat system/system/apex/com.android.vndk.current.apex.* 2>/dev/null >> system/system/apex/com.android.vndk.current.apex
+rm -f system/system/apex/com.android.vndk.current.apex.* 2>/dev/null
+cat system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null >> system/system/priv-app/MiuiVideo/MiuiVideo.apk
+rm -f system/system/priv-app/MiuiVideo/MiuiVideo.apk.* 2>/dev/null
+cat system/system/priv-app/Browser/Browser.apk.* 2>/dev/null >> system/system/priv-app/Browser/Browser.apk
+rm -f system/system/priv-app/Browser/Browser.apk.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/app/MiuiWallpaper/MiuiWallpaper.apk.* 2>/dev/null >> system/system/app/MiuiWallpaper/MiuiWallpaper.apk
+rm -f system/system/app/MiuiWallpaper/MiuiWallpaper.apk.* 2>/dev/null
+cat system/system/lib64/librelight_only_extraphoto.so.* 2>/dev/null >> system/system/lib64/librelight_only_extraphoto.so
+rm -f system/system/lib64/librelight_only_extraphoto.so.* 2>/dev/null
+cat system/system/data-app/Youpin/Youpin.apk.* 2>/dev/null >> system/system/data-app/Youpin/Youpin.apk
+rm -f system/system/data-app/Youpin/Youpin.apk.* 2>/dev/null
+cat system/system/data-app/com.ss.android.ugc.aweme_15/com.ss.android.ugc.aweme_15.apk.* 2>/dev/null >> system/system/data-app/com.ss.android.ugc.aweme_15/com.ss.android.ugc.aweme_15.apk
+rm -f system/system/data-app/com.ss.android.ugc.aweme_15/com.ss.android.ugc.aweme_15.apk.* 2>/dev/null
+cat system/system/data-app/MiShop/MiShop.apk.* 2>/dev/null >> system/system/data-app/MiShop/MiShop.apk
+rm -f system/system/data-app/MiShop/MiShop.apk.* 2>/dev/null
+cat system/system/data-app/com.taobao.taobao_24/com.taobao.taobao_24.apk.* 2>/dev/null >> system/system/data-app/com.taobao.taobao_24/com.taobao.taobao_24.apk
+rm -f system/system/data-app/com.taobao.taobao_24/com.taobao.taobao_24.apk.* 2>/dev/null
